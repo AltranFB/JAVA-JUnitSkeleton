c33fe717cfc9ace95b9ad218e3a690375809d85d
Grokwik <frederic@bodin.site>
2017-01-09 11:56:45 +0100

    Skeleton

---
+++ src/TestCases.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestCases {
+	@Test
+	public void test1() {
+		assertEquals("must return 1", 1, Solution.DoIt());
+	}
+
+	@Test
+    public void Test2() {
+    }
+
+	@Test
+	public void Test3() {
+    }
+
+	@Test
+	public void Test4() {
+    }
+
+	@Test
+	public void Test5() {
+    }
+
+	@Test
+	public void Test6() {
+    }
+}
